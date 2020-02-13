4f0956cd23048273322323b3ba779d55c3a9ca50
Mubashra <30776537+Mubashira-Wali@users.noreply.github.com>
2020-02-13 20:47:59 +0500

    Add files via upload

---
+++ health_checks.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+import shutil
+import psutil
+
+from network import *
+
+def check_disk_usage(disk):
+	du = shutil.disk_usage(disk)
+	free = du.free / du.total*100
+	return free > 5
+
+def check_cpu_usage():
+	usage = psutil.cpu_percent(1)
+	return usage < 75
+
+if not check_disk_usage('/') or not check_cpu_usage():
+    print("ERROR!")
+elif check_localhost() and check_connectivity():
+    print("Everything ok")
+else:
+    print("Network connections failed")
+
---
+++ network.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+import requests
+import socket
+
+def check_localhost():
+ localhost = socket.gethostbyname('localhost')
+ return localhost == '127.0.0.1'
+
+def check_connectivity():
+ request = requests.get("http://www.google.com")
+ response = request
+ response.status_code
+ return response.status_code == 200
+
